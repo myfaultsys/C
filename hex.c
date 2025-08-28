f2aeebcece0b1dfce6b31d05f14b6e729d17551d
Catman <lee.klese@icloud.com>
2025-08-28 22:28:08 +0200

    bug fix and added comments
    fixed mistake increment in for-loop in line 38 from "i++" to "i--"

--- hex.c
+++ hex.c
@@ -2,42 +2,55 @@
 #include<stdlib.h>
 #include<string.h>
 
+    // converts an integer into hexadecimal using the horner scheme
 char *hex(int n)
-{
+{   
+    // look-up table for hex-characters; buffer for storing digits
     static const char digits[] = "0123456789ABCDEF";
     static char hex[19];
     char *ptr = &hex[0];
+    // handle edge case
     if (n == 0)
     {
         *(ptr++) = '0';
         *ptr = '\0';
         return hex;
     }
+    // divide by 16 repeatedly and store remainders (our hex digits) into buffer
     while (n > 0)
     {
         int remainder = n % 16;
+    // store digit sequentually to array and increment pointer aftwerwards
         *(ptr++) = digits[remainder];
         n /= 16;
     }
-    int len = ptr -hex;
+    // don't forget to end string with NULL byte ;)
+    *ptr = '\0';
+    // total number of bytes used gives length of string
+    int len = ptr - hex;
+    // reverse string; temporarily store current one; switch up to half only
     for (int j = 0; j < len / 2; j++)
     {
         char temp = hex[j];
         hex[j] = hex[len - j - 1];
         hex[len - j - 1] = temp;
     }
-    *ptr = '\0';
     return hex;
 }
 
 int dec(const char *hex)
-{
-   int base = 1;
-   int result = 0;
-   int length = strlen(hex);
-   for (int i = length - 1; i >= 0; i++)
-    {
+{   
+    // base is 16**0 we start there and just multiply
+    int base = 1;
+    // initialize a result to modify
+    int result = 0;
+    // string length gives highest power of 16 to consider
+    int length = strlen(hex);
+    for (int i = length - 1; i >= 0; i--)
+    {   
+        // start at furthest right cgaracter (one less than length becauswe of NULL terminator)
         char current_char = hex[i];
+        // sequentally check cases and multiply to decimal number
         if (current_char >= '0' && current_char <= '9')
         {
             result += (current_char - '0') * base;
@@ -52,12 +65,12 @@ int dec(const char *hex)
         }
          else
         {
-            printf("Invalid Character: %c", current_char);
+            printf("Invalid Character: (%c)\n", current_char);
             return 0;
         }
         base *= 16;
     }
-   return result;
+    return result;
 }
 
 int main(int argc, char *argv[]) 
@@ -68,9 +81,10 @@ int main(int argc, char *argv[])
         return 1;
     }
     else
-    {
-        char * hexNum = hex(atoi(argv[1]));
-        printf("| dec: %d | hex: %s |\n", atoi(argv[1]), hexNum);
+    {   
+        char *hexNum = hex(atoi(argv[1]));
+        int dec_ret = dec(hexNum);
+        printf("| dec: %d | hex: %s |\n", dec_ret, hexNum);
         return 0;
     }
 }
